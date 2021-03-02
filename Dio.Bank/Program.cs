6b7c5a07432a3889820046c765bb1121050bb9bc
julioCoutinho <juliocezar0002@gmail.com>
2021-03-02 10:48:53 -0300

    Adicionando algumas aulas de dotnet realizadas

---
+++ Dio.Bank/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Dio.Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            
+
+        }
+
+        private static string ObterOpcaoUsuario(){
+            Console.WriteLine();
+            Console.WriteLine("DIO Bank a seu dispor!!!");
+            Console.WriteLine("Informe a opção desejada: ");
+
+            Console.WriteLine("1 - Listar contas");
+            Console.WriteLine("2 - inserir nova conta");
+            Console.WriteLine("3 - Transferir");
+            Console.WriteLine("4 - Sacar");
+            Console.WriteLine("5 - Depositar");
+            Console.WriteLine("6 - Limpar Tela");
+            Console.WriteLine("X - Sair");
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
---
+++ SistemaCalculoSalario/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace SistemaCalculaSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario, reajuste, novoSalario, percentual;
+            salario = Convert.ToDouble(Console.ReadLine());
+
+            //insira os valores corretos de acordo com o enunciado
+
+            if(salario >= 0 && salario <= 400)
+            {
+                reajuste = salario * 0.15; 
+                novoSalario = salario + reajuste;
+                percentual = ((novoSalario - salario) * 100) /salario;
+                Console.WriteLine("Novo salario: {0:0.00}", novoSalario);
+                Console.WriteLine("Reajuste ganho: {0:0.00}", reajuste);
+                if(salario == 0){
+                  Console.WriteLine("Em percentual: 15 %");
+                }else{
+                  Console.WriteLine("Em percentual: {0:0} %", percentual);
+                }
+            }
+            else if (salario > 400 && salario <= 800)
+            {
+                reajuste = salario * 0.12;
+                novoSalario = salario + reajuste;
+                percentual = ((novoSalario - salario) * 100) / salario;
+                Console.WriteLine("Novo salario: {0:0.00}", novoSalario);
+                Console.WriteLine("Reajuste ganho: {0:0.00}", reajuste);
+                Console.WriteLine("Em percentual: {0:0} %", percentual);
+            }
+            else if (salario > 800 && salario <= 1200)
+            {
+                reajuste = salario * 0.10;
+                novoSalario = salario + reajuste;
+                percentual = ((novoSalario - salario) * 100) / salario;
+                Console.WriteLine("Novo salario: {0:0.00}", novoSalario);
+                Console.WriteLine("Reajuste ganho: {0:0.00}", reajuste);
+                Console.WriteLine("Em percentual: {0:0} %", percentual);
+            }
+            else if (salario > 1200 && salario <= 2000)
+            {
+                reajuste = salario * 0.07;
+                novoSalario = salario + reajuste;
+                percentual = ((novoSalario - salario) * 100) / salario;
+                Console.WriteLine("Novo salario: {0:0.00}", novoSalario);
+                Console.WriteLine("Reajuste ganho: {0:0.00}", reajuste);
+                Console.WriteLine("Em percentual: {0:0} %", percentual);
+            }
+            else
+            {
+                reajuste = salario * 0.04;
+                novoSalario = salario + reajuste;
+                percentual = ((novoSalario - salario) * 100) / salario;
+                Console.WriteLine("Novo salario: {0:0.00}", novoSalario);
+                Console.WriteLine("Reajuste ganho: {0:0.00}", reajuste);
+                Console.WriteLine("Em percentual: {0:0} %", percentual);
+            }
+        }
+
+    }
+}
